04dac45e1460aad5ef81a81b604251cb2a32934d
Cyrille Kuete <cyrillekuete@gmail.com>
2021-11-05 11:02:21 +0100

    State management

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "lets hit the beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr its cold!",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "Winter" : "sumer";
+  }
+};
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  );
-};
-ReactDom.render(<App />, document.querySelector("#root"));
-// export default App;
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+  componentDidUpdate() {
+    console.log("Component was Updated Successfully");
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
